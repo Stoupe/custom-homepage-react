3eb834e5e0c64f262f5ee926a8bcc1a13777dca9
Henry Stoupe <h.stoupe@gmail.com>
2021-10-27 11:44:23 +1300

    Fix font issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,4 @@
 // import { Theme, ThemeProvider } from "@emotion/react";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-import "@fontsource/poppins";
 import {
   createTheme,
   responsiveFontSizes,
